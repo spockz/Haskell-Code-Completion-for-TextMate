d042bcfed2abcc61671dad8354cf63c0a575501c
Alessandro Vermeulen <avermeulen@spockz.nl>
2011-03-26 21:30:18 +0100

    The first commit.

---
+++ Main.hs
@@ -0,0 +1,26 @@
+module Main where
+  
+-- import Control.Monad ( (<<) )
+
+import System( getArgs )
+import System.IO( stderr, hPutStrLn )
+
+import System.Process( runCommand, waitForProcess)
+
+import Data.List( nub, sort, isPrefixOf )
+  
+main = do args <- getArgs
+          if (length args >= 2) then
+              do  let file = head args
+                  let current_word = (head . tail) args
+                  procHandle <- runCommand $ command file
+                  exitcode <- waitForProcess procHandle
+                  
+                  text <- readFile "/tmp/textmatetags"
+                  mapM_ (putStrLn) $ sort . nub -- . filter (isPrefixOf current_word) 
+                                   $ map (head . words) (lines text)
+                                   
+                  
+            else
+              hPutStrLn stderr "Provide a haskell file and a current word!"
+  where command file = "echo \":ctags /tmp/textmatetags\" | ghci " ++ file ++" &> /tmp/runtags"
